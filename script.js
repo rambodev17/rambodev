f060eb9bb9fdc0ac87c0068fd79aeb64a1c6d0f1
rambodev17 <devfivem.19@gmail.com>
2024-11-10 16:33:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("show-bouquet-btn").addEventListener("click", function() {
+    const bouquet = document.getElementById("bouquet");
+    bouquet.classList.toggle("show");
+});
